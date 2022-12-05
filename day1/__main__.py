8c95a188ae39d2aac5199ed5035b2527c90460b6
Aleksandr Borovkov <xander27b@gmail.com>
2022-12-05 08:20:20 +0000

    day1 part 2 done

--- day1/__main__.py
+++ day1/__main__.py
@@ -1,25 +1,46 @@
 from dataclasses import dataclass
 from functools import reduce
+from os import path
 import unittest
+
+
+class LimitedSortedList:
+    """The first elment is the max"""
+
+    def __init__(self, limit):
+        self.limit = limit
+        self.elements = []
+
+    def append(self, element): # TODO functional
+        inserted = False
+        for i, other in enumerate(self.elements):
+            if element > other:
+                self.elements.insert(i, element)
+                inserted = True
+                break
+
+        if inserted:
+            self.elements = self.elements[0:self.limit]
+        elif len(self.elements) < self.limit:
+            self.elements.append(element)
 
 
 @dataclass
 class State:
-    max: int
+    top3: LimitedSortedList
     cur_sum: int
 
     def apply(self, element):
         if element is None:
-            if self.cur_sum > self.max:
-                return State(self.cur_sum, 0)
-            else:
-                return State(self.max, 0)
+           self.top3.append(self.cur_sum) # TODO functional
+           return State(self.top3, 0)
         else:
-            return State(self.max, self.cur_sum + element)
+            return State(self.top3, self.cur_sum + element)
 
 
 def read_data(fname):
-    with open(fname, "r", encoding="utf-8") as file:
+    norm_file_name = path.join(path.dirname(__file__), fname)
+    with open(norm_file_name, "r", encoding="utf-8") as file:
         for line in file:
             line = line.rstrip()
             if len(line) > 0:
@@ -29,13 +50,35 @@ def read_data(fname):
 
 
 def solve(data):
-    result = reduce(lambda s, e: s.apply(e), data, State(0, 0))
+    result = reduce(lambda s, e: s.apply(e), data, State(LimitedSortedList(3), 0))
     result = result.apply(None)
-    return result.max
+    top3 = result.top3.elements
+    return (top3[0], sum(top3))
 
 
 def solve_file(fname):
     return solve(read_data(fname))
+
+
+class TestLimitedSortedList(unittest.TestCase):
+
+    def test_append_to_empty(self):
+        self._do_test([1], 1)
+
+    def test_apend_dsc(self):
+        self._do_test([5, 4, 3], 5, 4, 3, 2, 1)
+
+    def test_apend_asc(self):
+        self._do_test([5, 4, 3], 1, 2, 3, 4, 5)
+
+    def test_apend_random(self):
+        self._do_test([10, 5, 5], 1, 5, 4, 4, 5, 3, 10, 1, 2)
+
+    def _do_test(self, expected, *vargs):
+        sample = LimitedSortedList(3)
+        for x in vargs:
+            sample.append(x)
+        self.assertEqual(sample.elements, expected)
 
 
 class TestDay(unittest.TestCase):
@@ -51,11 +94,11 @@ class TestDay(unittest.TestCase):
     def test_solve(self):
         data = [1000, 2000, 3000, None, 4000, None, 5000,
                 6000, None, 7000, 8000, 9000, None, 10000]
-        self.assertEqual(solve(data), 24000)
+        self.assertEqual(solve(data), (24000, 45000))
 
     def test_solve_file(self):
         data = read_data("input-test.txt")
-        self.assertEqual(solve(data), 24000)
+        self.assertEqual(solve(data), (24000, 45000))
 
 
 if __name__ == '__main__':
