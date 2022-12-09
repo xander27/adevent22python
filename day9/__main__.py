4a864dde844f5ff52e2316e35965fa1d4cc5c658
Aleksandr Borovkov <xander27b@gmail.com>
2022-12-09 13:15:11 +0000

    day 9 part 2

--- day9/__main__.py
+++ day9/__main__.py
@@ -44,7 +44,6 @@ def read_commands(fname):
 def get_next_tail_position(cur_tail, head):
     diff = (head[0] - cur_tail[0], head[1] - cur_tail[1])
     abs_diff = (abs(diff[0]), abs(diff[1]))
-        
 
     if abs_diff[0] > 1:
         if abs_diff[1] == 0:
@@ -54,23 +53,29 @@ def get_next_tail_position(cur_tail, head):
 
     if abs_diff[1] > 1:
         if abs_diff[0] == 0:
-            return (cur_tail[0], cur_tail[1]  + normalize(diff[1]))
+            return (cur_tail[0], cur_tail[1] + normalize(diff[1]))
         else:
             return (cur_tail[0] + normalize(diff[0]), cur_tail[1] + normalize(diff[1]))
-    
+
     return cur_tail
 
-def count_tail_positions(commands):
+
+def count_tail_positions(commands, length):
     positions = set()
-    tail, head = (0, 0), (0, 0)
-    positions.add(tail)
+    rope = [(0, 0)] * length
+    positions.add(rope[-1])
 
     for command in commands:
         diff = command.direction.diff
         for _ in range(command.distance):
+            head = rope[0]
             head = (head[0] + diff[0], head[1] + diff[1])
-            tail = get_next_tail_position(tail, head)
-            positions.add(tail)
+            rope[0] = head
+            for i in range(length - 1):
+                head, tail = rope[i], rope[i + 1]
+                tail = get_next_tail_position(tail, head)
+                rope[i], rope[i+1] = head, tail
+            positions.add(rope[-1])
 
     return len(positions)
 
@@ -83,9 +88,9 @@ def normalize(value):
     return 0
 
 
-def score_file(fname):
+def score_file(fname, length):
     commands = read_commands(fname)
-    return count_tail_positions(commands)
+    return count_tail_positions(commands, length)
 
 
 class TestDay(unittest.TestCase):
@@ -101,18 +106,37 @@ class TestDay(unittest.TestCase):
         Command(Direction.RIGHT, 2)
     ]
 
+    COMMANDS_LARGE = [
+        Command(Direction.RIGHT, 5),
+        Command(Direction.UP, 8),
+        Command(Direction.LEFT, 8),
+        Command(Direction.DOWN, 3),
+        Command(Direction.RIGHT, 17),
+        Command(Direction.DOWN, 10),
+        Command(Direction.LEFT, 25),
+        Command(Direction.UP, 20)
+    ]
+
     def teat_count_tail_positions(self):
-        self.assertEqual(count_tail_positions(self.COMMANDS), 13)
+        self.assertEqual(count_tail_positions(self.COMMANDS, 2), 13)
+        self.assertEqual(count_tail_positions(self.COMMANDS_LARGE, 10), 36)
 
     def read_commands(self):
         self.assertSequenceEqual(
-            list(read_commands("input-test.txt")), self.COMMANDS)
+            list(read_commands("input-test.txt")),
+            self.COMMANDS
+        )
+        self.assertSequenceEqual(
+            list(read_commands("input-test-lage.txt")),
+            self.COMMANDS_LARGE
+        )
 
     def test_score_filee(self):
-        self.assertEqual(score_file("input-test.txt"), 13)
+        self.assertEqual(score_file("input-test.txt", 2), 13)
+        self.assertEqual(score_file("input-test-large.txt", 10), 36)
 
 
 if __name__ == '__main__':
-    print(score_file("input.txt"))
+    print((score_file("input.txt", 2), score_file("input.txt", 10)))
     print("=====")
     unittest.main()
