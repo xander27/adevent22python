3cb07ee62bf2394c014e3f4b5e4949016e320c61
Aleksandr Borovkov <xander27b@gmail.com>
2022-12-11 09:41:32 +0000

    day 11 part 2

--- day11/__main__.py
+++ day11/__main__.py
@@ -5,10 +5,50 @@ import unittest
 
 CALM_FACTOR = 3
 
+@dataclass
+class Item():
+    value: int
+    dividers: set[int]
+    bases: set[int]
+
+    def __init__(self, value, bases, dividers=None):
+        self.bases = bases
+        self.b = 1
+        for base in bases:
+            self.b = base * self.b
+        self.value = value % self.b
+        self.dividers = self._find_dividers(value) if dividers is None else dividers
+
+    def __add__(self, other):
+        return Item(self.value + other, self.bases)
+
+    def __mul__(self, other):
+        other_dividers = self._find_dividers(other)
+        other_dividers.update(self.dividers)
+        return Item(self.value*other, self.bases, other_dividers)
+
+    def __pow__(self, other):
+        return Item(self.value**other, self.bases, self.dividers)
+
+    def __mod__(self, other):
+        return 0 if other in self.dividers else 1
+
+    def _find_dividers(self, value):
+        dividers = set()
+        while value & 1 == 0:
+            value = value >> 1
+            dividers.add(2)
+        for base in self.bases:
+            result, rem = divmod(value, base)
+            if rem == 0:
+                dividers.add(base)
+                value = result
+        return dividers
+
 
 @dataclass
 class Monkey():
-    items: list[int]
+    items: list[Item]
     operation: str
     operation_arg: int
     divider: int
@@ -23,10 +63,12 @@ def remove_prefix(string, prefix):
         raise BaseException(f"{string} is not started with {prefix}")
     return string[len(prefix):]
 
+def parse_base(line):
+    return int(remove_prefix(line, "Test: divisible by "))
 
-def parse_monkey(lines):
+def parse_monkey(lines, bases):
     items_str = remove_prefix(lines[1], "Starting items: ")
-    items = list(map(int, items_str.split(", ")))
+    items = list(map(lambda s: Item(int(s), bases), items_str.split(", ")))
 
     operation_str = remove_prefix(lines[2], "Operation: new = old ")
     if operation_str == "* old":
@@ -48,7 +90,9 @@ def parse_monkey(lines):
 
 
 def parse_monkeys(lines):
-    return [parse_monkey(lines[i: i + 6]) for i in range(0, len(lines), 7)]
+    bases = set((parse_base(lines[i + 3]) for i in range(0, len(lines), 7)))
+    monkeys = [parse_monkey(lines[i: i + 6], bases) for i in range(0, len(lines), 7)]
+    return monkeys
 
 
 def read_lines(fname):
@@ -69,16 +113,16 @@ def apply_operation(value, operation, arg):
     elif operation == "*":
         return value * arg
     elif operation == "**" and arg == 2:
-        return value * value
+        return value ** 2
     else:
         raise BaseException(f"Unkown operation {operation} for arg {arg}")
 
 
-def calculate_new_value(value, operation, operation_arg, relif):
-    value = apply_operation(value, operation, operation_arg)
+def calculate_new_value(item, operation, operation_arg, relif):
+    item = apply_operation(item, operation, operation_arg)
     if relif:
-        value = value // CALM_FACTOR
-    return value
+        item = Item(item.value // CALM_FACTOR, item.bases)
+    return item
 
 
 def simulate_round(monkeys, relif):
@@ -95,7 +139,8 @@ def simulate_round(monkeys, relif):
 
 def business_level(monkeys, relif, rounds_number):
     for i in range(rounds_number):
-        print(f"{i}/{rounds_number}")
+        if i % 50 == 0:
+            print(f"{i}/{rounds_number}")
         simulate_round(monkeys, relif)
     all_inspects = map(lambda m: m.inspects, monkeys)
     top2 = sorted(all_inspects)[-2:]
@@ -103,50 +148,55 @@ def business_level(monkeys, relif, rounds_number):
 
 
 def score_file(fname):
-    monkeys = read_monkeys(fname)
     return (
-        business_level(monkeys, True, 20),
-        business_level(monkeys, False, 10000),
+        business_level(read_monkeys(fname), True, 20),
+        business_level(read_monkeys(fname), False, 10000),
     )
 
 
 class TestDay(unittest.TestCase):
 
-    def _get_inital_monkeys(self):
+    BASES = set([23, 19, 17, 13])
+
+    def _get_inital(self):
         return [
-            Monkey([79, 98], "*", 19, 23, 2, 3),
-            Monkey([54, 65, 75, 74], "+", 6, 19, 2, 0),
-            Monkey([79, 60, 97], "**", 2, 13, 1, 3),
-            Monkey([74], "+", 3, 17, 0, 1),
+            Monkey(self._as_items(79, 98), "*", 19, 23, 2, 3),
+            Monkey(self._as_items(54, 65, 75, 74), "+", 6, 19, 2, 0),
+            Monkey(self._as_items(79, 60, 97), "**", 2, 13, 1, 3),
+            Monkey(self._as_items(74), "+", 3, 17, 0, 1),
         ]
 
     def test_read_monkeys(self):
-        expected = self._get_inital_monkeys()
+        expected = self._get_inital()
         self.assertSequenceEqual(read_monkeys("input-test.txt"), expected)
 
     def test_apply_operation(self):
-        self.assertEqual(apply_operation(5, "+", 2), 7)
-        self.assertEqual(apply_operation(5, "*", 3), 15)
-        self.assertEqual(apply_operation(5, "**", 2), 25)
+        self.assertEqual(apply_operation(Item(5, self.BASES), "+", 2), Item(7, self.BASES))
+        self.assertEqual(apply_operation(Item(5, self.BASES), "*", 3), Item(15, self.BASES))
+        self.assertEqual(apply_operation(Item(5, self.BASES), "**", 2), Item(25, self.BASES))
 
     def test_simulate_round(self):
-        monkeys = self._get_inital_monkeys()
+        monkeys = self._get_inital()
         simulate_round(monkeys, True)
-        self.assertSequenceEqual(monkeys[0].items, [20, 23, 27, 26])
         self.assertSequenceEqual(
-            monkeys[1].items, [2080, 25, 167, 207, 401, 1046])
+            monkeys[0].items, self._as_items(20, 23, 27, 26))
+        self.assertSequenceEqual(
+            monkeys[1].items, self._as_items(2080, 25, 167, 207, 401, 1046))
         self.assertSequenceEqual(monkeys[2].items, [])
         self.assertSequenceEqual(monkeys[3].items, [])
 
+    def _as_items(self, *numbers):
+        return list(map(lambda x: Item(x, self.BASES), numbers))
+
     def test_business_level(self):
-        self.assertEqual(business_level(
-            self._get_inital_monkeys(), True, 20), 10605)
+        self.assertEqual(business_level(self._get_inital(), True, 20), 10605)
+        self.assertEqual(business_level(self._get_inital(), False, 10000), 2713310158)
 
     def test_score_file(self):
         self.assertEqual(score_file("input-test.txt"), (10605, 2713310158))
 
 
 if __name__ == '__main__':
-    # print(score_file("input.txt"))
+    print(score_file("input.txt"))
     print("=====")
     unittest.main()
