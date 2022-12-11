187d0427bbb5281382fd57a7bd8e21aada43a300
Aleksandr Borovkov <xander27b@gmail.com>
2022-12-11 06:31:59 +0000

    day11 part 1

--- day11/__main__.py
+++ day11/__main__.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from os import path
 import unittest
 
+CALM_FACTOR = 3
+ROUNDS_NUMBER = 20
 
 @dataclass
 class Monkey():
@@ -11,7 +13,8 @@ class Monkey():
     operation_arg: int
     divider: int
     true_path: int
-    false_path: int
+    false_path: int 
+    inspects: int = 0
 
 
 def remove_prefix(string, prefix):
@@ -60,17 +63,78 @@ def read_monkeys(fname):
     return parse_monkeys(lines)
 
 
+def apply_operation(value, operation, arg):
+    if operation == "+":
+        return value + arg
+    elif operation == "*":
+        return value * arg
+    elif operation == "**" and arg == 2:
+        return value * value
+    else:
+        raise BaseException(f"Unkown operation {operation} for arg {arg}")
+
+
+def calculate_new_value(value, operation, operation_arg):
+    return apply_operation(value, operation, operation_arg) // CALM_FACTOR
+
+
+def simulate_round(monkeys):
+    for monkey in monkeys:
+        items = monkey.items
+        monkey.inspects = monkey.inspects + len(items)
+        monkey.items = []
+        for item in items:
+            value = calculate_new_value(item, monkey.operation, monkey.operation_arg)  
+            going = monkey.true_path if value % monkey.divider == 0 else monkey.false_path
+            monkeys[going].items.append(value)
+
+def business_level(monkeys):
+    for _ in range(ROUNDS_NUMBER):
+        simulate_round(monkeys)
+    all_inspects = map(lambda m: m.inspects, monkeys)
+    top2 = sorted(all_inspects)[-2:]
+    return top2[0] * top2[1]
+
+def score_file(fname):
+    monkeys = read_monkeys(fname)
+    return business_level(monkeys)
+
+
 class TestDay(unittest.TestCase):
 
-    def test_read_monkeys(self):
-        expected = [
+    def _get_inital_monkeys(self):
+        return [
             Monkey([79, 98], "*", 19, 23, 2, 3),
             Monkey([54, 65, 75, 74], "+", 6, 19, 2, 0),
             Monkey([79, 60, 97], "**", 2, 13, 1, 3),
             Monkey([74], "+", 3, 17, 0, 1),
         ]
+
+    def test_read_monkeys(self):
+        expected = self._get_inital_monkeys()
         self.assertSequenceEqual(read_monkeys("input-test.txt"), expected)
+
+    def test_apply_operation(self):
+        self.assertEqual(apply_operation(5, "+", 2), 7)
+        self.assertEqual(apply_operation(5, "*", 3), 15)
+        self.assertEqual(apply_operation(5, "**", 2), 25)
+
+    def test_simulate_round(self):
+        monkeys = self._get_inital_monkeys()
+        simulate_round(monkeys)
+        self.assertSequenceEqual(monkeys[0].items, [20, 23, 27, 26])
+        self.assertSequenceEqual(monkeys[1].items, [2080, 25, 167, 207, 401, 1046])
+        self.assertSequenceEqual(monkeys[2].items, [])
+        self.assertSequenceEqual(monkeys[3].items, [])
+
+    def test_business_level(self):
+        self.assertEqual(business_level(self._get_inital_monkeys()), 10605)
+
+    def test_score_file(self):
+        self.assertEqual(score_file("input-test.txt"), 10605)
 
 
 if __name__ == '__main__':
+    print(score_file("input.txt"))
+    print("=====")
     unittest.main()
