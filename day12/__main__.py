336b7c9823ebec7549cf308d70daf6ee76ba10bb
borovkov <a.borovkov@cft.ru>
2022-12-12 16:30:33 +0700

    day 12 part 2

--- day12/__main__.py
+++ day12/__main__.py
@@ -4,8 +4,8 @@ from os import path
 import unittest
 
 BASE_ORD = ord('a')
-START_CHAR = 'S'
-END_CHAR = 'E'
+START_CHAR = 'E'
+END_CHAR = 'S'
 MAX_HEIGHT = ord('z') - BASE_ORD
 
 
@@ -13,14 +13,14 @@ MAX_HEIGHT = ord('z') - BASE_ORD
 class MapGraph():
     heights: list[list[int]]
     start: tuple[int, int]
-    end: tuple[int, int]
+    ends: set[tuple[int, int]]
     rows: int
     cols: int
 
-    def __init__(self, heights, start, end):
+    def __init__(self, heights, start, ends):
         self.heights = heights
         self.start = start
-        self.end = end
+        self.ends = ends
         self.rows = len(heights)
         self.cols = len(heights[0])
 
@@ -28,28 +28,31 @@ class MapGraph():
     def can_go(self, from_point, to_point):
         from_height = self.heights[from_point[0]][from_point[1]]
         to_height = self.heights[to_point[0]][to_point[1]]
-        return to_height <= from_height + 1
+        return to_height >= from_height - 1
 
 
-def parse_map_graph(lines):
+def parse_map_graph(lines, many_ends):
     heights = []
     start = None
-    end = None
+    ends = set()
 
     for i, line in enumerate(lines):
         heights_line = []
         for j, char in enumerate(line):
             if char == START_CHAR:
-                heights_line.append(0)
+                heights_line.append(MAX_HEIGHT)
                 start = (i, j)
             elif char == END_CHAR:
-                heights_line.append(MAX_HEIGHT)
-                end = (i, j)
+                heights_line.append(0)
+                ends.add((i, j))
             else:
-                heights_line.append(ord(char) - BASE_ORD)
+                height = ord(char) - BASE_ORD
+                heights_line.append(height)
+                if many_ends and height == 0:
+                    ends.add((i, j))
         heights.append(heights_line)
 
-    return MapGraph(heights, start, end)
+    return MapGraph(heights, start, ends)
 
 def find_way(graph):
     init_distance = graph.rows * graph.cols + 1
@@ -66,21 +69,14 @@ def find_way(graph):
     while len(todo) > 0:
         pointer = pop_next(todo, distance, init_distance)
         cur_dist = distance[pointer[0]][pointer[1]]
-        if graph.end == pointer:
-            for g in graph.heights:
-                print(g)
-            print("=====")
-            for d in distance:
-                print(d)
-            return cur_dist
         to_go = find_points_to_go(pointer, graph, complete, distance)
         for other in to_go:
             if distance[other[0]][other[1]] > cur_dist + 1:
                 distance[other[0]][other[1]] = cur_dist + 1
         complete[pointer[0]][pointer[1]] = True
         todo.update(to_go)
-    
-    raise BaseException("Can't find the way")
+
+    return min(distance[e[0]][e[1]] for e in graph.ends)
 
 def pop_next(todo, distance, init_distance):
     min_distance = init_distance
@@ -117,15 +113,13 @@ def read_lines(fname):
         for line in file:
             yield line.rstrip()
 
-def read_map_graph(fname):
-    return parse_map_graph(read_lines(fname))
-
 def solve_file(fname):
-    return find_way(read_map_graph(fname))
+    lines = list(read_lines(fname))
+    return (find_way(parse_map_graph(lines, False)), find_way(parse_map_graph(lines, True)))
 
 class TestDay(unittest.TestCase):
 
-    MAP_GRAPH = MapGraph(
+    MAP_GRAPH_ONE_END = MapGraph(
         heights=[
             [0, 0, 1, 16, 15, 14, 13, 12],
             [0, 1, 2, 17, 24, 23, 23, 11],
@@ -133,8 +127,20 @@ class TestDay(unittest.TestCase):
             [0, 2, 2, 19, 20, 21, 22, 9],
             [0, 1, 3, 4, 5, 6, 7, 8]
         ],
-        start=(0, 0),
-        end=(2,5)
+        start=(2, 5),
+        ends=set([(0,0)])
+    )
+
+    MAP_GRAPH_MANY_ENDS = MapGraph(
+        heights=[
+            [0, 0, 1, 16, 15, 14, 13, 12],
+            [0, 1, 2, 17, 24, 23, 23, 11],
+            [0, 2, 2, 18, 25, 25, 23, 10],
+            [0, 2, 2, 19, 20, 21, 22, 9],
+            [0, 1, 3, 4, 5, 6, 7, 8]
+        ],
+        start=(2, 5),
+        ends=set([(0,0), (0,1), (1,0), (2,0), (3,0), (4, 0)])
     )
 
     def test_parse_map_graph(self):
@@ -145,13 +151,12 @@ class TestDay(unittest.TestCase):
             "acctuvwj",
             "abdefghi"
         ]
-        self.assertEqual(parse_map_graph(lines), self.MAP_GRAPH)
-
-    def test_read_map_graph(self):
-        self.assertEqual(read_map_graph("input-test.txt"), self.MAP_GRAPH)
+        self.assertEqual(parse_map_graph(lines, False), self.MAP_GRAPH_ONE_END)
+        self.assertEqual(parse_map_graph(lines, True), self.MAP_GRAPH_MANY_ENDS)
 
     def test_find_way(self):
-        self.assertEqual(find_way(self.MAP_GRAPH), 31)
+        self.assertEqual(find_way(self.MAP_GRAPH_ONE_END), 31)
+        self.assertEqual(find_way(self.MAP_GRAPH_MANY_ENDS), 29)
 
 if __name__ == '__main__':
     print(solve_file("input.txt"))
