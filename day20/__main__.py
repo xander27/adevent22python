91be5527ab612aaf0d32f1c36586347e4e8f68ac
borovkov <a.borovkov@cft.ru>
2022-12-20 14:16:54 +0700

    day 20 with duplicates broken

--- day20/__main__.py
+++ day20/__main__.py
@@ -9,21 +9,20 @@ def read_numbers(fname):
     with open(norm_file_name, "r", encoding="utf-8") as file:
         return list(map(int, file.read().split("\n")))
 
-def positions_to_list(positions):
+def positions_to_list(original, positions):
     result = [0] * len(positions)
-    print(len(positions), max(positions.values()))
-    for n, p in positions.items():
+    for i, n in enumerate(original):
+        p = positions[i]
         result[p] = n
     return result
 
 def reoder(numbers):
     length = len(numbers)
-    positions = { n: i for i, n in enumerate(numbers) }
-    print(positions_to_list(positions))
-    for number in numbers:
+    positions = { i: i for i in range(length) }
+    for i, number in enumerate(numbers):
         if number == 0: 
             continue
-        cur_pos = positions[number]
+        cur_pos = positions[i]
         new_pos = cur_pos + number
         if number < 0:
             new_pos -= 1
@@ -34,14 +33,18 @@ def reoder(numbers):
             positions = { n: p - 1 if p > cur_pos and p <= new_pos else p for n, p in positions.items() }
         else:
             positions = { n: p + 1 if p < cur_pos and p >= new_pos else p for n, p in positions.items() }
-        positions[number] = new_pos
-        # print(positions_to_list(positions))
+        positions[i] = new_pos
+        # print(positions_to_list(numbers, positions))
         # print(list(enumerate(positions_to_list(positions))))
-    return positions_to_list(positions), positions
+    return positions_to_list(numbers, positions), positions
     
 def solve(numbers):
     reordered, positions = reoder(numbers)
-    p0 = positions[0]
+    p0 = -1
+    for i, n in enumerate(reordered):
+        if n == 0:
+            p0 = i
+            break
     positions = [ (i * 1000 + p0) % len(positions) for i in range(1, 4) ]
     return sum(reordered[p] for p in positions)
 
