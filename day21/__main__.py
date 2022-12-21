b012db228861f595f87643d5e5a0f6662165d147
borovkov <a.borovkov@cft.ru>
2022-12-21 13:38:51 +0700

    day 21 part 2 not working

--- day21/__main__.py
+++ day21/__main__.py
@@ -4,6 +4,38 @@ from dataclasses import dataclass
 from os import path
 import unittest
 
+ROOT = "root"
+ME = "humn"
+
+
+@dataclass
+class Fraction:
+    top: int
+    bottom: int
+
+    def __add__(self, other):
+        return Fraction(self.top * other.bottom + self.bottom * other.top, self.bottom * other.bottom)
+
+    def __sub__(self, other):
+        return Fraction(self.top * other.bottom - self.bottom * other.top, self.bottom * other.bottom)
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Fraction(self.top * other, self.bottom)
+        return Fraction(self.top * other.top, self.bottom * other.bottom)
+
+    def __truediv__(self, other):
+        return Fraction(self.top * other.bottom, self.bottom * other.top)
+
+    def as_int(self):
+        return self.top // self.bottom
+
+
+@dataclass
+class Effect:
+    a: int
+    b: int
+
 
 @dataclass
 class NumberMonkey():
@@ -12,6 +44,11 @@ class NumberMonkey():
     def calc(self, data):
         return self.value
 
+    def get_effect(self, data, name):
+        if name == ME:
+            return Effect(Fraction(1, 1), Fraction(0, 1))
+        return Effect(Fraction(0, 1), Fraction(self.value, 1))
+
 
 @dataclass
 class OperationMonkey():
@@ -19,6 +56,7 @@ class OperationMonkey():
     operation: str
     second: str
     value: int = None
+    effect: str = None
 
     def calc(self, data):
         if self.value is None:
@@ -36,6 +74,36 @@ class OperationMonkey():
                 raise BaseException(f"Unknown operation {self.operation}")
         return self.value
 
+    def get_effect(self, data, name):
+        if self.effect is None:
+            first_effect = data[self.first].get_effect(data, self.first)
+            second_effect = data[self.second].get_effect(data, self.second)
+            if self.operation == "+" or self.operation == "-":
+                mul = 1 if self.operation == "+" else -1
+                self.effect = Effect(
+                    first_effect.a + second_effect.a * mul, first_effect.b + second_effect.b * mul)
+            elif self.operation == "*" or self.operation == "/":
+                if first_effect.a.top != 0 and second_effect.a.top != 0:
+                    raise BaseException(f"Cant do powers")
+                if first_effect.a.top == 0 and second_effect.a.top == 0:
+                    self.effect = Effect(
+                        Fraction(0, 1), Fraction(self.calc(data), 1))
+                if first_effect.a.top != 0:
+                    primary = first_effect
+                    secondary = second_effect.b
+                else:
+                    primary = second_effect
+                    secondary = first_effect.b
+                if self.operation == "*":
+                    self.effect = Effect(
+                        primary.a * secondary, primary.b * secondary)
+                else:
+                    self.effect = Effect(
+                        primary.a / secondary, primary.b / secondary)
+            else:
+                raise BaseException(f"Unknown operation {self.operation}")
+        return self.effect
+
 
 def parse_monkey(line):
     name, body = line.strip().split(": ")
@@ -44,6 +112,7 @@ def parse_monkey(line):
         return name, NumberMonkey(int(body_parts[0]))
     else:
         return name, OperationMonkey(*body_parts)
+
 
 def read_file(fname):
     result = {}
@@ -54,9 +123,32 @@ def read_file(fname):
             result[name] = monkey
     return result
 
+
+def find_what_to_yell(data):
+    root = data[ROOT]
+    root_first = data[root.first]
+    root_second = data[root.second]
+
+    first_branch = root_first.get_effect(data, root.first)
+    second_branch = root_second.get_effect(data, root.second)
+
+    if first_branch.a != 0:
+        x_branch = first_branch
+        other_branch = second_branch
+    else:
+        other_branch = first_branch
+        x_branch = second_branch
+    print(x_branch, other_branch)
+    result = (other_branch.b - x_branch.b)/x_branch.a
+    return result.as_int()
+
+
 def solve_file(fname):
     data = read_file(fname)
-    return data["root"].calc(data)
+    p1 = data[ROOT].calc(data)
+    p2 = find_what_to_yell(data)
+    return p1, p2
+
 
 class TestDay(unittest.TestCase):
 
@@ -68,7 +160,7 @@ class TestDay(unittest.TestCase):
         self.assertEqual(parse_monkey("dbpl: 5"), ("dbpl", NumberMonkey(5)))
 
     def test_solve_file(self):
-        self.assertEqual(solve_file("input-test.txt"), 152)
+        self.assertEqual(solve_file("input-test.txt"), (152, 301))
 
 
 if __name__ == '__main__':
